4d91b398b28816a4514c0f91e657693191b228a6
krishpranav <krisna.pranav@gmail.com>
2021-06-26 16:25:55 +0530

    [+] options [+]

--- pkg/tcpserver/tcpserver.go
+++ pkg/tcpserver/tcpserver.go
@@ -1,3 +1,13 @@
 package tcpserver
 
 const readTimeout = 5
+
+type Options struct {
+	Listen      string
+	TLS         bool
+	Certificate string
+	Key         string
+	Domain      string
+	rules       []Rule
+	Verbose     bool
+}
