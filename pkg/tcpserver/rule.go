1a5c37fbf2deb35a63cfe005009cd3e37daa5ae9
krishpranav <krisna.pranav@gmail.com>
2021-06-26 16:32:01 +0530

    [+] rule [+]

--- pkg/tcpserver/rule.go
+++ pkg/tcpserver/rule.go
@@ -2,14 +2,21 @@ package tcpserver
 
 import "regexp"
 
-// RulesConfiguration from yaml
 type RulesConfiguration struct {
 	Rules []Rule `yaml:"rules"`
 }
 
-// Rule to apply to various requests
 type Rule struct {
 	Match      string `yaml:"match,omitempty"`
 	matchRegex *regexp.Regexp
 	Response   string `yaml:"response,omitempty"`
+}
+
+func NewRule(match, response string) (*Rule, error) {
+	regxp, err := regexp.Compile(match)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Rule{Match: match, matchRegex: regxp, Response: response}, nil
 }
