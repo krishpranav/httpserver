7dfd45e7b6e5678abd5819a4084bec367d36cac3
krishpranav <krisna.pranav@gmail.com>
2021-06-26 16:28:21 +0530

    [+] Listen And Serve [+ ]

--- pkg/tcpserver/tcpserver.go
+++ pkg/tcpserver/tcpserver.go
@@ -27,3 +27,12 @@ func (t *TCPServer) AddRule(rule Rule) error {
 	t.options.rules = append(t.options.rules, rule)
 	return nil
 }
+
+func (t *TCPServer) ListenAndServe() error {
+	listener, err := net.Listen("tcp4", t.options.Listen)
+	if err != nil {
+		return err
+	}
+	t.listener = listener
+	return t.run()
+}
