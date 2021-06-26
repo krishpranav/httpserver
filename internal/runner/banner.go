0cd68761fdf2b89bc499267dffdb7b5a61bca878
krishpranav <krisna.pranav@gmail.com>
2021-06-26 16:36:34 +0530

    [+] banner [+]

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -1,5 +1,14 @@
 package runner
 
+import "github.com/projectdiscovery/gologger"
+
 const banner = `SIMPLE HTTP SERVER`
 
 const Version = `1.0.0`
+
+func showBanner() {
+	gologger.Print().Msgf("%s\n", banner)
+
+	gologger.Print().Msgf("Use with caution. You are responsible for your actions\n")
+	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
+}
